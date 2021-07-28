7b8334a12ae01b2f1670297d4305c229d5e7c370
Daria Peydus <dasha_vishnya@protonmail.com>
2021-07-28 21:09:31 +0300

    Add Java code

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long balance = 2_000_000_000;
+        long transfer = 500_000_000;
+        long total = balance + transfer;
+        System.out.println(total);
+    }
+}
